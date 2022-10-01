9d31e9ed72f49de5641516c8f10670949ab73ea0
jbeltran402 <juandavid72009@gmail.com>
2022-10-01 02:30:46 -0500

    Using QueryMethods to catch specific information or Optionals from DB

--- src/main/java/com/juan/tienda/persistence/ProductoRepository.java
+++ src/main/java/com/juan/tienda/persistence/ProductoRepository.java
@@ -1,6 +1,7 @@
 package com.juan.tienda.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.juan.tienda.persistence.crud.ProductoCrudRepository;
 import com.juan.tienda.persistence.entity.Producto;
@@ -10,5 +11,17 @@ public class ProductoRepository {
     
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getBycategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrdeByNombreAsc(idCategoria);
+    }
+
+    public List<Producto> getBycategoriaQuery(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaQuery(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(Integer cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/juan/tienda/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/juan/tienda/persistence/crud/ProductoCrudRepository.java
@@ -1,8 +1,19 @@
 package com.juan.tienda.persistence.crud;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.juan.tienda.persistence.entity.Producto;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer>{
+
+    @Query(value = "SELECT * FROM productos WHERE id_categoria + ?", nativeQuery = true)
+    List<Producto> findByIdCategoriaQuery(Integer idCategoria);
+
+    List<Producto> findByIdCategoriaOrdeByNombreAsc(Integer idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(Integer cantidadStock,boolean estado);
 }
